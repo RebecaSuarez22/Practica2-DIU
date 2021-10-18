a8d216870ec19b433571344f7da201b4d6500637
RebecaSuarez22 <72138219+RebecaSuarez22@users.noreply.github.com>
2021-10-18 19:41:15 +0100

    Add files via upload

---
+++ practica2_DIU/src/practica2_diu/Practica2_DIU.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica2_diu;
+
+public class Practica2_DIU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conversor x =new Conversor();
+        x.setVisible(true);
+    }
+    
+}
